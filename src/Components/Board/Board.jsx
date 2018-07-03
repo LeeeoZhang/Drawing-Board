f5e9b45337e8b979f1e4613224168d3a764c9242
leo Cheung <leozhang621@gmail.com>
2018-07-04 00:14:55 +0800

    start project

---
+++ src/Components/Board/Board.jsx
@@ -0,0 +1,14 @@
+import React  from 'react'
+import styles from  './Board.scss'
+export default class Board extends React.Component {
+
+	render () {
+		return (
+			<div className={styles["board-wrap"]}>
+
+			</div>
+		)
+	}
+
+}
+
